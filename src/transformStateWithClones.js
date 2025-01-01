efc15483297e44de5ad944ce47c8eaba0c0534c5
DagmaraProkop <dagmara.prokop95@gmail.com>
2025-01-01 13:26:16 +0100

    Poprawki funkcji transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,29 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
-}
+const stateHistory = [];
+const stateChange = {};
 
+for (const key in state) {
+  stateChange[key] = state[key];
+} // copy state object to stateChange
+
+function transformStateWithClones(stateChange, actions) {
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(stateChange, key.extraData);
+      stateHistory.push(stateChange);
+    } else if (key.type === 'removeProperties') {
+      for (const key1 of key.keysToRemove) {
+        delete stateChange[key1];
+        stateHistory.push(stateChange);
+      }
+    } else if (key.type === 'clear') {
+      for (const key2 in stateChange) {
+        delete stateChange[key2];
+        stateHistory.push(stateChange);
+      }
+    }
+  }
+}
 module.exports = transformStateWithClones;
